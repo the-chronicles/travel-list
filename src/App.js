a22b0989b5af11fbccd55963b3d55cfc7c609205
the-chronicles <oluseungideon@gmail.com>
2023-10-19 04:55:24 +0100

    Used controlled elements

--- src/App.js
+++ src/App.js
@@ -23,16 +23,23 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, if: Date.now() };
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
